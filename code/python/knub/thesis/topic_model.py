72c12a834f3691a777fcdd75931a93402d4d5afd
Stefan Bunk <stefan.bunk@googlemail.com>
2016-06-22 18:37:25 +0200

    Do not use multi-processed LDA.

--- code/python/knub/thesis/topic_model.py
+++ code/python/knub/thesis/topic_model.py
@@ -11,8 +11,8 @@ def main():
     id2word = gensim.corpora.Dictionary.load_from_text(bz2.BZ2File("/data/wikipedia/2016-06-21/gensim_wordids.txt.bz2"))
     mm = gensim.corpora.MmCorpus("/data/wikipedia/2016-06-21/gensim_tfidf.mm")
     print mm
-    # lda = gensim.models.ldamulticore.LdaMulticore(corpus=mm, num_topics=100, id2word=id2word, chunksize=10000, passes=1)
-    lda = gensim.models.ldamulticore.LdaMulticore(corpus=mm, num_topics=100, id2word=id2word, workers=3)
+    lda = gensim.models.ldamodel.LdaModel(corpus=mm, num_topics=100, id2word=id2word, chunksize=10000, passes=1)
+    # lda = gensim.models.ldamodel.LdaModel(corpus=mm, num_topics=100, id2word=id2word, workers=3)
     lda.save("/data/wikipedia/2016-06-21/topics.model")
     lda.print_topics()
     logging.info("Finished Wikipedia LDA")
