9ca77ef9da8f658496fd6a37696687c6cbfd2293
Stefan Bunk <stefan.bunk@googlemail.com>
2016-06-25 11:47:05 +0200

    Add better assertion failed exception

--- code/scala/src/main/scala/knub/master_thesis/WikiPlainTextIterator.scala
+++ code/scala/src/main/scala/knub/master_thesis/WikiPlainTextIterator.scala
@@ -1,26 +1,30 @@
 package knub.master_thesis
 
-import java.nio.file.{Files, Paths}
+import java.nio.file.{Files, Path, Paths}
 
 import cc.mallet.types.Instance
+
 import scala.collection.JavaConverters._
 
 class WikiPlainTextIterator(dataFolderName: String) extends java.util.Iterator[Instance] {
 
     val p = Paths.get(dataFolderName)
-    val fileIterator = java.nio.file.Files.walk(p).iterator().asScala.filter(Files.isRegularFile(_))
+    val pathIterator = java.nio.file.Files.walk(p).iterator().asScala.filter(Files.isRegularFile(_))
     var lineIterator: Iterator[String] = null
+    var currentPath: Path = null
     val sb = new StringBuilder
 
     override def hasNext: Boolean = {
-        fileIterator.hasNext || lineIterator.hasNext
+        pathIterator.hasNext || lineIterator.hasNext
     }
 
     override def next(): Instance = {
-        if (lineIterator == null || !lineIterator.hasNext)
-            lineIterator = scala.io.Source.fromFile(fileIterator.next.toFile).getLines()
+        if (lineIterator == null || !lineIterator.hasNext) {
+            currentPath = pathIterator.next()
+            lineIterator = scala.io.Source.fromFile(currentPath.toFile).getLines()
+        }
         val firstLine = lineIterator.next
-        assert(firstLine.startsWith("<doc"))
+        assert(firstLine.startsWith("<doc"), s"$currentPath: \"$firstLine\" does not start with <doc")
 
         sb.clear()
         var currentLine = ""
