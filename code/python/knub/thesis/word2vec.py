74860bc035873c00d4bcfe649dbac369dd9054ba
Stefan Bunk <stefan.bunk@googlemail.com>
2016-06-27 10:42:01 +0200

    Train word2vec using gensim

--- code/python/knub/thesis/word2vec.py
+++ code/python/knub/thesis/word2vec.py
@@ -1,8 +1,25 @@
+import argparse
+import logging
+
 from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
 
-def sentences():
-    yield 'I am awesome in the house to be'
-    yield 'You are awesome too in the awesome'
-    yield 'I like you awesome awesome'
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-model = Word2Vec(sentences(), size=100, window=5, min_count=0, workers=1)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Training word2vec with gensim")
+    parser.add_argument("sentences", type=file)
+    parser.add_argument("model", type=file)
+    parser.add_argument("threads", type=int)
+    args = parser.parse_args()
+
+    logging.info("Training word2vec")
+    sentences = LineSentence(args.sentences)
+    model = Word2Vec(sentences, size=100, window=5, min_count=5, workers=args.threads, sg=True)
+    model.save(args.model)
+    logging.info("Finished training word2vec")
+
+    logging.info(model.most_similar(positive=['woman', 'king'], negative=['man']))
+    logging.info(model.doesnt_match("breakfast cereal dinner lunch".split()))
+    logging.info(model.similarity('woman', 'man'))
+
