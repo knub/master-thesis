7b890ae240f7b5f4090a46a8f794623e5102583d
Stefan Bunk <stefan.bunk@googlemail.com>
2016-07-03 10:46:47 +0200

    Fix word2vec eval

--- code/python/knub/thesis/word2vec_eval.py
+++ code/python/knub/thesis/word2vec_eval.py
@@ -44,4 +44,4 @@ if __name__ == "__main__":
             if most_similar[0] == task.answer:
                 correct_count += 1
 
-    print correct_count * 100.0 / task_count
+        print correct_count * 100.0 / task_count
