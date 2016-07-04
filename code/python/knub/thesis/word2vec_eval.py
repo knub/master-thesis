8a83f9f1a84758d78e05964c867cf817098c642a
Stefan Bunk <stefan.bunk@googlemail.com>
2016-07-04 11:19:41 +0200

    Use only two cores for word2vec eval

--- code/python/knub/thesis/word2vec_eval.py
+++ code/python/knub/thesis/word2vec_eval.py
@@ -1,13 +1,10 @@
-
 import argparse
 import logging
-from gensim.models.word2vec import Word2Vec
-
 from collections import namedtuple
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+from gensim.models.word2vec import Word2Vec
 
-#word2vec = gensim.models.Word2Vec.load_word2vec_format(GLOVE_VECTOR_FILE, binary=False)
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Evaluating word2vec with analogy task")
@@ -18,6 +15,7 @@ if __name__ == "__main__":
         print model
 
         word2vec = Word2Vec.load_word2vec_format(model, binary=True)
+        word2vec.workers = 2
 
         f = open("../../../../data/analogy-reasoning/questions-words.txt")
         AnalogyTask = namedtuple("AnalogyTask", "plus1 minus1 plus2 answer")
@@ -32,15 +30,16 @@ if __name__ == "__main__":
         correct_count = 0
         task_count = 0
         for task in analogy_tasks:
-            s = "%s - %s + %s = %s" % (task.plus1, task.minus1, task.plus2, task.answer)
-
-            most_similars = word2vec.most_similar(positive=[task.plus1, task.plus2], negative=[task.minus1], topn=4)
-            ignore_words = { task.plus1, task.plus2, task.minus1 }
-            most_similar = next((word, prob) for word, prob in most_similars if not word in ignore_words)
-
-            task_count += 1
             if task_count % 1000 == 0:
                 print task_count,
+
+            # s = "%s - %s + %s = %s" % (task.plus1, task.minus1, task.plus2, task.answer)
+
+            most_similars = word2vec.most_similar(positive=[task.plus1, task.plus2], negative=[task.minus1], topn=4)
+            ignore_words = {task.plus1, task.plus2, task.minus1}
+            most_similar = next((word, prob) for word, prob in most_similars if word not in ignore_words)
+
+            task_count += 1
             if most_similar[0] == task.answer:
                 correct_count += 1
 
