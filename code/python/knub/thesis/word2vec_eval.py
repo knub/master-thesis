b00f1972df2c78e0c3668168dcc6b0e51cd6177a
Stefan Bunk <stefan.bunk@googlemail.com>
2016-07-05 23:43:04 +0200

    Fix word2vec eval path.

--- code/python/knub/thesis/word2vec_eval.py
+++ code/python/knub/thesis/word2vec_eval.py
@@ -8,7 +8,7 @@ from gensim.models.word2vec import Word2Vec
 
 
 def eval_word2vec(word2vec):
-    f = open("../../../../data/analogy-reasoning/questions-words.txt")
+    f = open("/home/stefan.bunk/master-thesis/data/analogy-reasoning/questions-words.txt")
     AnalogyTask = namedtuple("AnalogyTask", "plus1 minus1 plus2 answer")
     analogy_tasks = []
     for l in f:
