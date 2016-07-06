41de6d36a7ac0d67555b83c3c75da2e23675dcda
Stefan Bunk <stefan.bunk@googlemail.com>
2016-07-06 21:49:05 +0200

    Run different things

--- code/scala/src/main/scala/knub/master_thesis/Main.scala
+++ code/scala/src/main/scala/knub/master_thesis/Main.scala
@@ -96,14 +96,14 @@ object Main {
     def analyzeResult(res: TopicModelResult, args: Args): Unit = {
         val frequentWords = Source.fromFile("../../data/vocab.txt").getLines().toArray
 
-        println("Write top words")
-        writeTopWordsToTextFile(res, args)
-        println("Concept categorization")
-        conceptCategorization(res, args)
-//        println("Write topic probs")
-//        val topicProbs = writeTopicProbsToFile(res, args, frequentWords.toSet)
-//        println("Find word pairs")
-//        findWordPairs(res, args, frequentWords, topicProbs)
+//        println("Write top words")
+//        writeTopWordsToTextFile(res, args)
+//        println("Concept categorization")
+//        conceptCategorization(res, args)
+        println("Write topic probs")
+        val topicProbs = writeTopicProbsToFile(res, args, frequentWords.toSet)
+        println("Find word pairs")
+        findWordPairs(res, args, frequentWords, topicProbs)
 
         /*
          * WHAT TO DO:
