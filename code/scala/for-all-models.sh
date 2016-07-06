b2728012737731cfb49d62da6182ba9c390159ed
Stefan Bunk <stefan.bunk@googlemail.com>
2016-07-06 14:25:29 +0200

    Add for all models script

---
+++ code/scala/for-all-models.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for filename in /data/wikipedia/2016-06-21/topic-models/*.model; do
+      model_name=$(basename $filename)
+      log_name="$model_name.log"
+      echo $model_name
+      bin/topic-model topic-model --model-file-name $filename | tee $log_name
+done
