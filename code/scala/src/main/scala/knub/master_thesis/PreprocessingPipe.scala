58a42f1fa90665c1b61db5ed3e9ce53b145dbdf8
Stefan Bunk <stefan.bunk@googlemail.com>
2016-07-07 18:44:51 +0200

    Use 2000 first words of document

--- code/scala/src/main/scala/knub/master_thesis/PreprocessingPipe.scala
+++ code/scala/src/main/scala/knub/master_thesis/PreprocessingPipe.scala
@@ -5,7 +5,7 @@ import java.util.regex.Pattern
 
 import cc.mallet.pipe.{Pipe, SerialPipes}
 import cc.mallet.pipe._
-import knub.master_thesis.preprocessing.UseFixedVocabulary
+import knub.master_thesis.preprocessing.{UseFixedVocabulary, UseOnlyFirstNWordsOfDocument}
 
 import scala.collection.mutable
 import scala.collection.JavaConverters._
@@ -20,10 +20,10 @@ object PreprocessingPipe {
             // Regex explanation: lowercase [lowercase punctuation]+ lowercase  --- at least three characters,
             // no punctuation at the end
             new CharSequence2TokenSequence(Pattern.compile("\\p{L}[\\p{L}\\p{P}]+\\p{L}")),
-//            new UseOnlyFirstNWordsOfDocument(1000),
+            new UseOnlyFirstNWordsOfDocument(2000),
             new TokenSequenceRemoveStopwords(
                 new File(stopWordsFileName), "UTF-8", false, false, false),
-            new UseFixedVocabulary(vocabulary),
+//            new UseFixedVocabulary(vocabulary),
             new TokenSequence2FeatureSequence()
         )
     }
