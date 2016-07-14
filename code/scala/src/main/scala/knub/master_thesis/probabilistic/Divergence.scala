b5d06ed5d817769fb75617f49b856badd434536d
Stefan Bunk <stefan.bunk@googlemail.com>
2016-07-14 13:06:18 +0200

    More efficient implementations for divergences

--- code/scala/src/main/scala/knub/master_thesis/probabilistic/Divergence.scala
+++ code/scala/src/main/scala/knub/master_thesis/probabilistic/Divergence.scala
@@ -15,21 +15,40 @@ object Divergence {
       * The smaller, the more similar. Zero means exact
       */
     def maxDistance(p: Array[Double], q: Array[Double]): Double = {
-        p.indices.map { i => Math.abs(p(i) - q(i)) }.max
+        var max = 0.0
+        var i = 0
+        while (i < p.length) {
+            val diff = Math.abs(p(i) - q(i))
+            if (diff > max)
+                max = diff
+            i += 1
+        }
+        max
     }
     /**
       * The smaller, the more similar. Zero means exact
       */
     def sumDistance(p: Array[Double], q: Array[Double]): Double = {
-        0.5 * p.indices.map { i => Math.abs(p(i) - q(i)) }.sum
+        var sum = 0.0
+        var i = 0
+        while (i < p.length) {
+            sum += Math.abs(p(i) - q(i))
+            i += 1
+        }
+        0.5 * sum
     }
 
     /**
       * The smaller, the more similar. Zero means exact
       */
     def bhattacharyyaDistance(p: Array[Double], q: Array[Double]): Double = {
-        val BC = p.indices.map { i => Math.sqrt(p(i) * q(i)) }.sum
-        - Math.log(BC)
+        var sum = 0.0
+        var i = 0
+        while (i < p.length) {
+            sum += Math.sqrt(p(i) * q(i))
+            i += 1
+        }
+        - Math.log(sum)
     }
 
     val sqrt2Rez = 1.0 / Math.sqrt(2)
@@ -37,21 +56,14 @@ object Divergence {
       * The smaller, the more similar. Zero means exact
       */
     def hellingerDistance(p: Array[Double], q: Array[Double]): Double = {
-        val innerSum = p.indices.map { i =>
+        var sum = 0.0
+        var i = 0
+        while (i < p.length) {
             val sqrtDiff = Math.sqrt(p(i)) - Math.sqrt(q(i))
-            sqrtDiff * sqrtDiff
-        }.sum
-        sqrt2Rez * Math.sqrt(innerSum)
+            sum += sqrtDiff * sqrtDiff
+            i += 1
+        }
+        sqrt2Rez * Math.sqrt(sum)
     }
-
-
-//    def kullbackLeibler(p: Array[Double], q: Array[Double]): Double = {
-//        var sum = 0.0
-//        for (i <- p.indices) {
-//            if (p(i) != 0.0 && q(i) != 0.0)
-//                sum += p(i) * Math.log(p(i) / q(i))
-//        }
-//        sum
-//    }
 
 }
