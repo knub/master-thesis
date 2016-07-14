76c28e4099bde5160171f03a2dad32d735e3237d
Stefan Bunk <stefan.bunk@googlemail.com>
2016-07-14 15:09:27 +0200

    Generate word2vec similarity sample

--- code/python/knub/thesis/word2vec_coherence.py
+++ code/python/knub/thesis/word2vec_coherence.py
@@ -1,8 +1,10 @@
 import argparse
 import logging
+import numpy as np
 
 import mkl
 from gensim.models.word2vec import Word2Vec
+
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     mkl.set_num_threads(3)
@@ -18,22 +20,39 @@ if __name__ == "__main__":
 
     word2vec = Word2Vec.load_word2vec_format(args.embedding_model, binary=True)
 
-    with open(args.topic_model, "r") as input:
-        with open(args.embedding_model + ".ssv", "w") as output:
-            for line in input:
-                if "topic-count" not in line: # skip first header line
-                    split = line.split(" ")
+    # with open(args.topic_model, "r") as input:
+    #     with open(args.embedding_model + ".ssv", "w") as output:
+    #         for line in input:
+    #             if "topic-count" not in line: # skip first header line
+    #                 split = line.split(" ")
+    #
+    #                 i = 2
+    #                 word_found = False
+    #                 while not word_found:
+    #                     try:
+    #                         best_topic_word = split[2] # first is topic, second is topic count, third is first word
+    #                         most_similars = word2vec.most_similar([best_topic_word], topn=9)
+    #                         most_similars = map(lambda t: t[0], most_similars)
+    #                         most_similars = map(lambda s: s.encode('utf8'), most_similars)
+    #                         most_similars.insert(0, best_topic_word)
+    #                         output.write("%s\n" % " ".join(most_similars))
+    #                         word_found = True
+    #                     except:
+    #                         i += 1
 
-                    i = 2
-                    word_found = False
-                    while not word_found:
-                        try:
-                            best_topic_word = split[2] # first is topic, second is topic count, third is first word
-                            most_similars = word2vec.most_similar([best_topic_word], topn=9)
-                            most_similars = map(lambda t: t[0], most_similars)
-                            most_similars = map(lambda s: s.encode('utf8'), most_similars)
-                            most_similars.insert(0, best_topic_word)
-                            output.write("%s\n" % " ".join(most_similars))
-                            word_found = True
-                        except:
-                            i += 1
+    frequent_words = [line.rstrip('\n') for line in open("/data/wikipedia/2016-06-21/vocab.txt")]
+    frequent_words = frequent_words[10000:-40000]
+    frequent_words = list(enumerate(frequent_words))
+    print len(frequent_words)
+    random_sample = np.random.choice(frequent_words, 100000, replace=False)
+    random_sample = sorted(frequent_words, key=lambda i, _: i)
+
+
+    with open(args.embedding_model + ".similars", "w") as output:
+        for _, word in random_sample:
+            try:
+                similars = word2vec.most_similar([word], topn=3)
+                for word, prob in similars:
+                    output.write("%s\t%s" % (word, prob))
+            except:
+                pass
