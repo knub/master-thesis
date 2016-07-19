cd4abba3311df63eee98566330e07a927497eac5
Stefan Bunk <stefan.bunk@googlemail.com>
2016-07-19 22:48:26 +0200

    Train word2vec with trigrams

--- code/python/knub/thesis/word2vec.py
+++ code/python/knub/thesis/word2vec.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 
 from gensim.models import Word2Vec
+from gensim.models.phrases import Phrases
 from gensim.models.word2vec import LineSentence
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -15,7 +16,9 @@ if __name__ == "__main__":
 
     logging.info("Training word2vec")
     sentences = LineSentence(args.sentences)
-    model = Word2Vec(sentences, size=200, window=5, min_count=50, workers=args.threads, sg=False, hs=0, negative=5, sample=0.001)
+    bigrams = Phrases(sentences, 20, 40)
+    trigrams = Phrases(bigrams, 5, 10)
+    model = Word2Vec(trigrams[sentences], size=256, window=5, min_count=50, workers=args.threads, sg=True, hs=0, negative=10, sample=0.001)
     model.save_word2vec_format(args.model, binary=True)
     logging.info("Finished training word2vec")
 
