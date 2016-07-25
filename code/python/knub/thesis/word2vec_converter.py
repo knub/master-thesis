8892cde59c94f0d51288fb4c49a0f20e80cee5bc
Stefan Bunk <stefan.bunk@googlemail.com>
2016-07-25 17:58:56 +0200

    Also store vocabulary in word2vec converter.

--- code/python/knub/thesis/word2vec_converter.py
+++ code/python/knub/thesis/word2vec_converter.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     model = Word2Vec.load_word2vec_format(args.model, binary=True)
-    model.save_word2vec_format(args.model + ".txt", binary=False)
+    model.save_word2vec_format(args.model + ".txt", fvocab=args.model + ".vocab", binary=False)
 
     logging.info(model.most_similar(positive=['woman', 'king'], negative=['man']))
     logging.info(model.doesnt_match("breakfast cereal dinner lunch".split()))
