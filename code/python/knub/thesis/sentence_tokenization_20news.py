e9736a0de0e16b877507cbd26d9a4ea6b9db48ad
Stefan Bunk <stefan.bunk@googlemail.com>
2016-07-25 14:02:37 +0200

    Tokenize 20news

---
+++ code/python/knub/thesis/sentence_tokenization_20news.py
@@ -0,0 +1,47 @@
+import argparse
+import codecs
+import os
+import string
+from itertools import dropwhile
+from random import shuffle
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+parser = argparse.ArgumentParser("Training word2vec with gensim")
+parser.add_argument("twentynews_folder", type=str)
+args = parser.parse_args()
+
+def get_filenames():
+    files = []
+    for dirpath, _, filenames in os.walk(args.twentynews_folder):
+        for filename in filenames:
+            files.append(dirpath + "/" + filename)
+    shuffle(files)
+    return files
+
+
+filenames = get_filenames()
+
+sentences_file = codecs.open(args.twentynews_folder + "/sentences.txt", "w", encoding="iso-8859-1")
+articles_file = codecs.open(args.twentynews_folder + "/articles.txt", "w", encoding="iso-8859-1")
+
+for filename in filenames:
+    with codecs.open(filename, "r", encoding="iso-8859-1") as f:
+        content = f.readlines()
+        body = dropwhile(lambda x: x != "\n", content)
+        body = " ".join(body)
+        sentences = sent_tokenize(body)
+        for sentence in sentences:
+            words = word_tokenize(sentence)
+            for word in words:
+                is_punctuation = all([s in string.punctuation for s in word])
+                if not is_punctuation:
+                    articles_file.write(word)
+                    articles_file.write(" ")
+                    sentences_file.write(word)
+                    sentences_file.write(" ")
+            sentences_file.write("\n")
+    articles_file.write("\n")
+
+sentences_file.close()
+articles_file.close()
