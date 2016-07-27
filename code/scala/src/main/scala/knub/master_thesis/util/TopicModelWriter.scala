f3aa4c1aa016795248dea6857a45e4dee643d5c3
Stefan Bunk <stefan.bunk@googlemail.com>
2016-07-27 17:13:34 +0200

    Try different topic word sorting.

--- code/scala/src/main/scala/knub/master_thesis/util/TopicModelWriter.scala
+++ code/scala/src/main/scala/knub/master_thesis/util/TopicModelWriter.scala
@@ -40,8 +40,9 @@ class TopicModelWriter(private val model: WordEmbeddingLDA) {
             writer.write(String.valueOf(tIndex))
             val topicWordProbs = mutable.Map[Int, Double]()
             for (wIndex <- 0 until model.vocabularySize) {
-                val pro = (model.topicWordCountLDA(tIndex)(wIndex) + params.beta) /
-                        (model.sumTopicWordCountLDA(tIndex) + model.betaSum)
+//                val pro = (model.topicWordCountLDA(tIndex)(wIndex) + params.beta) /
+//                        (model.sumTopicWordCountLDA(tIndex) + model.betaSum)
+                val pro = model.topicWordCountLDA(tIndex)(wIndex)
                 topicWordProbs(wIndex) = pro
             }
             val TOP_WORDS = 10
@@ -69,22 +70,22 @@ class TopicModelWriter(private val model: WordEmbeddingLDA) {
         writer.close()
     }
 
-    def writeDocTopicPros(name: String) {
-        val writer = new BufferedWriter(new FileWriter(params.modelFileName + ".welda-" + name + ".theta"))
-        for (i <- 0 until params.numDocuments) {
-            for (j <- 0 until params.numTopics) {
-                val pro = (model.docTopicCount(i)(j) + params.alpha) / (model.sumDocTopicCount(i) + model.alphaSum)
-                writer.write(pro + " ")
-            }
-            writer.write("\n")
-        }
-        writer.close()
-    }
+//    def writeDocTopicPros(name: String) {
+//        val writer = new BufferedWriter(new FileWriter(params.modelFileName + ".welda-" + name + ".theta"))
+//        for (i <- 0 until params.numDocuments) {
+//            for (j <- 0 until params.numTopics) {
+//                val pro = (model.docTopicCount(i)(j) + params.alpha) / (model.sumDocTopicCount(i) + model.alphaSum)
+//                writer.write(pro + " ")
+//            }
+//            writer.write("\n")
+//        }
+//        writer.close()
+//    }
 
     def write(name: String) {
         writeTopTopicalWords(name)
         if (name == model.p.numIterations.toString) {
-            writeDocTopicPros(name)
+//            writeDocTopicPros(name)
             writeTopicAssignments(name)
             writeTopicWordPros(name)
         }
