0df35e81754f703d1a8164cf0ea5169a53355185
Stefan Bunk <stefan.bunk@googlemail.com>
2016-08-02 16:38:34 +0200

    Fix parameter parsing in gaussian lda preprocessing.

--- code/python/knub/thesis/word2vec_gaussian_lda_preprocessing.py
+++ code/python/knub/thesis/word2vec_gaussian_lda_preprocessing.py
@@ -1,4 +1,5 @@
 import argparse
+from codecs import open
 import logging
 import os
 
@@ -8,16 +9,16 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Prepare model for Gaussian LDA")
-    parser.add_argument("--topic_model", type=str)
-    parser.add_argument("--embedding_model", type=str)
+    parser.add_argument("--topic-model", type=str)
+    parser.add_argument("--embedding-model", type=str)
     args = parser.parse_args()
 
     word2vec = Word2Vec.load_word2vec_format(args.embedding_model, binary=True)
     embedding_name = os.path.basename(args.embedding_model)
-    with open(args.topic_model + "." + embedding_name + ".gaussian-lda", "w") as output:
-        with open(args.topic_model + ".restricted.alphabet", "r") as f:
+    with open(args.topic_model + "." + embedding_name + ".gaussian-lda", "w", encoding="utf-8") as output:
+        with open(args.topic_model + "." + embedding_name + ".restricted.alphabet", "r", encoding="utf-8") as f:
             for line in f:
                 word = line.split("#")[0]
                 output.write(word + " ")
-                output.write(" ".join(word2vec[word]))
+                output.write(" ".join(map(str, word2vec[word])))
                 output.write("\n")
