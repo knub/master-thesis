72e4c202d714117d7827523c81ac6f2cbb74a4a4
Stefan Bunk <stefan.bunk@googlemail.com>
2016-08-25 14:57:06 +0200

    Introduce background topic.

--- code/scala/src/main/scala/knub/master_thesis/TopicModel.scala
+++ code/scala/src/main/scala/knub/master_thesis/TopicModel.scala
@@ -1,9 +1,12 @@
 package knub.master_thesis
 
+import java.util
+
 import cc.mallet.topics.ParallelTopicModel
 import cc.mallet.types.{Instance, InstanceList}
 
 class TopicModel(args: Args, alpha: Double, beta: Double, instancesIterator: java.util.Iterator[Instance]) {
+
 
     def run(stopWordsFileName: String): TopicModelResult = {
         val instances = new InstanceList(PreprocessingPipe.pipe(stopWordsFileName))
@@ -11,6 +14,7 @@ class TopicModel(args: Args, alpha: Double, beta: Double, instancesIterator: jav
 
         val numTopics = args.numTopics
         val model = new ParallelTopicModel(numTopics, numTopics * alpha, beta)
+        model.alpha = calculateAlphaWithBackgroundTopic(numTopics, alpha)
         model.setOptimizeInterval(10)
         model.setBurninPeriod(100)
         model.addInstances(instances)
@@ -20,5 +24,12 @@ class TopicModel(args: Args, alpha: Double, beta: Double, instancesIterator: jav
         model.estimate()
 
         new TopicModelResult(model)
+    }
+
+    def calculateAlphaWithBackgroundTopic(numTopics: Int, alpha: Double): Array[Double] = {
+        val a = new Array[Double](numTopics)
+        java.util.Arrays.fill(a, alpha)
+        a(0) = alpha * 5
+        a
     }
 }
