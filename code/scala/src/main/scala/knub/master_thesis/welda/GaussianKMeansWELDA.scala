1244b20a91a924232fbed222ed514418c5d56e24
Stefan Bunk <stefan.bunk@googlemail.com>
2016-10-12 17:35:30 +0200

    Add XMeans

--- code/scala/src/main/scala/knub/master_thesis/welda/GaussianKMeansWELDA.scala
+++ code/scala/src/main/scala/knub/master_thesis/welda/GaussianKMeansWELDA.scala
@@ -1,14 +1,24 @@
 package knub.master_thesis.welda
 
 import java.util
+import java.util.Date
 
-import cc.mallet.cluster.KMeans
-import cc.mallet.types.{Metric, SparseVector}
+import breeze.linalg.{DenseVector, NotConvergedException}
+import breeze.stats.distributions.MultivariateGaussian
 import knub.master_thesis.Args
-import weka.clusterers.SimpleKMeans
+import knub.master_thesis.util.Sampler
+import weka.clusterers.XMeans
 import weka.core._
 
+import scala.collection.mutable
+
+case class GaussianMixture(mixture: Array[Double], gaussians: Array[MultivariateGaussian])
+
 class GaussianKMeansWELDA(p: Args) extends GaussianWELDA(p) {
+
+    override val PCA_DIMENSIONS = 4
+
+    var gaussianMixtures: Array[GaussianMixture] = _
 
     override def getFolderName(): String = {
         s"${p.modelFileName}.$embeddingName.welda.gaussian-kmeans." +
@@ -16,37 +26,71 @@ class GaussianKMeansWELDA(p: Args) extends GaussianWELDA(p) {
             s"lambda-${LAMBDA.toString.replace('.', '-')}"
     }
 
-    override def getTopTopicVectors(): Array[Seq[Array[Double]]] = {
-//        val kmeans = new KMeans(null, 2)
-//        weka.clusterers.XMeans
-        val topTopicVectors = super.getTopTopicVectors()
-        topTopicVectors.map { topicVectors =>
-            val attributes = new util.ArrayList[Attribute]()
-            for (i <- 0 until p.pcaDimensions) {
-                attributes.add(new Attribute(i.toString))
-            }
-            val instances = new Instances("vectors", attributes, p.distributionEstimationSamples)
-            topicVectors.foreach { vector =>
-                val inst = new DenseInstance(1.0, vector)
-                instances.add(inst)
-            }
-            val NUM_CLUSTERS = 2
-            val kmeans = new SimpleKMeans()
-            kmeans.setNumClusters(NUM_CLUSTERS)
-            kmeans.setInitializationMethod(new SelectedTag(SimpleKMeans.KMEANS_PLUS_PLUS, SimpleKMeans.TAGS_SELECTION))
-            kmeans.setPreserveInstancesOrder(true)
-            kmeans.buildClusterer(instances)
 
-            val counts = new Array[Int](NUM_CLUSTERS)
-            val assignments = kmeans.getAssignments
-            assignments.foreach { a => counts(a) += 1 }
-            println(s"${counts(0)}-${counts(1)}")
-            val maxIndex = counts.zipWithIndex.maxBy(_._1)._2
-            val maxValue = counts(maxIndex)
-            if (maxValue >= 15)
-                topicVectors.zip(assignments).filter { case (_, a) => a == maxIndex }.map(_._1)
-            else
-                topicVectors
-        }
+    override def estimateDistributionParameters() = {
+        val topTopicVectors = getTopTopicVectors()
+
+        val clusterCount = mutable.Map[Int, Int]().withDefaultValue(0)
+        gaussianMixtures = topTopicVectors.indices.map { topicId =>
+            val topicVectors = topTopicVectors(topicId)
+            val (xmeans, instances) = clusterWords(topicVectors)
+
+            val NUM_CLUSTERS = xmeans.numberOfClusters()
+            clusterCount(NUM_CLUSTERS) += 1
+            var mixture = new Array[Double](NUM_CLUSTERS)
+            val assignments = (0 until instances.size()).map { i => xmeans.clusterInstance(instances.get(i)) }
+            assignments.foreach { a =>
+                mixture(a) += 1.0
+            }
+            if (NUM_CLUSTERS > 1)
+                println(s"\tTopic $topicId: ${mixture.deep}")
+
+            val gaussians = mixture.indices.flatMap { i =>
+                val vectors = topicVectors.zip(assignments).filter(_._2 == i).map(_._1)
+                val (meanVector, covarianceMatrix) = determineGaussianParameters(vectors)
+                try {
+                    Some(mixture(i), new MultivariateGaussian(meanVector, covarianceMatrix))
+                } catch {
+                    case e: NotConvergedException =>
+                        println(s"\tTopic $topicId-$i: ERROR - Did not converge for ${vectors.size} points")
+                        None
+                }
+            }.toArray
+            mixture = gaussians.map(_._1)
+            mixture.indices.foreach { i =>
+                mixture(i) /= mixture.sum
+            }
+            assert(Math.abs(mixture.sum - 1.0) < 0.0001, s"mixture should add to 1 but adds to ${mixture.sum}")
+            GaussianMixture(mixture, gaussians.map(_._2))
+        }.toArray
+        println(
+            s"\t\t${new Date}: " +
+                clusterCount.toList.sortBy(_._1).map { case (clusters, count) => s"$clusters cluster/s: $count"}.mkString(", "))
     }
+
+    def clusterWords(topicVectors: Seq[Array[Double]]): (XMeans, Instances) = {
+        val attributes = new util.ArrayList[Attribute]()
+        for (i <- 0 until PCA_DIMENSIONS) {
+            attributes.add(new Attribute(i.toString))
+        }
+        val instances = new Instances("vectors", attributes, DISTRIBUTION_ESTIMATION_SAMPLES)
+        topicVectors.foreach { vector =>
+            val inst = new DenseInstance(1.0, vector)
+            instances.add(inst)
+        }
+        val xmeans = new XMeans()
+        xmeans.setMaxIterations(10)
+        xmeans.setMinNumClusters(1)
+        xmeans.setMaxNumClusters(4)
+        xmeans.buildClusterer(instances)
+
+        (xmeans, instances)
+    }
+
+    override def sampleFromDistribution(topicId: Int): DenseVector[Double] = {
+        val gaussianMixture = gaussianMixtures(topicId)
+        val mixture = Sampler.nextDiscrete(gaussianMixture.mixture)
+        gaussianMixture.gaussians(mixture).sample()
+    }
+
 }
