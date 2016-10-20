475561e12cd0303fb98e9640da09f6252928f784
Stefan Bunk <stefan.bunk@googlemail.com>
2016-10-20 11:59:39 +0200

    Write out top word percentage

--- code/scala/src/main/scala/knub/master_thesis/util/TopicModelWriter.scala
+++ code/scala/src/main/scala/knub/master_thesis/util/TopicModelWriter.scala
@@ -12,7 +12,7 @@ class TopicModelWriter(private val model: BaseWELDA) {
 
     def baseName = model.fileBaseName
 
-    def writeParameters() {
+    def writeParameters(): Unit = {
         val writer = new BufferedWriter(new FileWriter(baseName + ".welda.params"))
         writer.write("-model" + "\t" + "WELDA")
         writer.write("\n-topicmodel" + "\t" + params.modelFileName)
@@ -24,7 +24,7 @@ class TopicModelWriter(private val model: BaseWELDA) {
         writer.close()
     }
 
-    def writeTopicAssignments(name: String) {
+    def writeTopicAssignments(name: String): Unit = {
         val writer = new BufferedWriter(new FileWriter(baseName + s".$name.topic-assignments"))
         for (dIndex <- 0 until params.numDocuments) {
             val docSize = model.corpusWords(dIndex).size
@@ -63,7 +63,7 @@ class TopicModelWriter(private val model: BaseWELDA) {
         writer.close()
     }
 
-    def writeTopicWordPros(name: String) {
+    def writeTopicWordPros(name: String): Unit = {
         val writer = new BufferedWriter(new FileWriter(baseName + s".$name.phi"))
         for (t <- 0 until model.numTopics) {
             for (w <- 0 until model.vocabularySize) {
@@ -76,11 +76,13 @@ class TopicModelWriter(private val model: BaseWELDA) {
         writer.close()
     }
 
-    def writeDocTopicProbs(name: String) {
+    def writeDocTopicProbs(name: String): Array[Array[Double]] = {
         val writer = new BufferedWriter(new FileWriter(baseName + s".$name.document-topics"))
+        val docTopicMatrix = Array.ofDim[Double](params.numDocuments, model.numTopics)
         for (i <- 0 until params.numDocuments) {
             for (j <- 0 until model.numTopics) {
                 val pro = (model.docTopicCount(i)(j) + params.alpha) / (model.docWordCount(i) + model.alphaSum)
+                docTopicMatrix(i)(j) = pro
                 if (j == 0)
                     writer.write(pro.toString)
                 else
@@ -89,16 +91,63 @@ class TopicModelWriter(private val model: BaseWELDA) {
             writer.write("\n")
         }
         writer.close()
+        docTopicMatrix
+    }
+
+    def writePercentageOfTopWords(name: String, docTopicMatrix: Array[Array[Double]]): Unit = {
+        val topWords = 10
+        val writer = new BufferedWriter(new FileWriter(baseName + s".$name.percentage-of-top-words"))
+
+        for (tIndex <- 0 until model.numTopics) {
+            writer.write(String.valueOf(tIndex))
+            val topicWordProbs = mutable.Map[Int, Double]()
+            for (wIndex <- 0 until model.vocabularySize) {
+                val pro = model.topicWordCountLDA(tIndex)(wIndex)
+                topicWordProbs(wIndex) = pro
+            }
+            val mostLikelyWords = topicWordProbs.toSeq.sortBy(-_._2).take(topWords).map(_._1)
+            for (wordId <- mostLikelyWords) {
+                writer.write(" " + model.id2WordVocabulary(wordId))
+            }
+            writer.write("\n")
+
+            val mostLikelyWordsSet = mostLikelyWords.toSet
+            // find topics where tIndex is prominent
+            val filtered = docTopicMatrix.zipWithIndex.filter { case (topicProbs, idx) =>
+                topicProbs.zipWithIndex.maxBy(_._1)._2 == tIndex
+            }.map(_._2)
+            if (filtered.isEmpty) {
+                writer.write("  Topic is not dominant topic anywhere\n")
+            } else {
+                var topWordCount = 0
+                var totalCount = 0
+                filtered.foreach { docId =>
+                    val doc = model.corpusWords(docId)
+                    totalCount += doc.size()
+                    var i = 0
+                    while (i < doc.size()) {
+                        val w = doc.get(i)
+                        if (mostLikelyWordsSet.contains(w)) {
+                            topWordCount += 1
+                        }
+                        i += 1
+                    }
+                }
+                writer.write(f"  $topWordCount / $totalCount = ${100 * topWordCount.toDouble / totalCount}%.2f %%\n")
+            }
+        }
+        writer.close()
     }
 
     def write(it: Int) {
         val name = f"iteration-$it%03d"
         writeTopTopicalWords(name)
-        writeDocTopicProbs(name)
+        val docTopicMatrix = writeDocTopicProbs(name)
         if (it == model.p.numIterations) {
             writeTopTopicalWords(name, 100)
             writeTopicAssignments(name)
             writeTopicWordPros(name)
+            writePercentageOfTopWords(name, docTopicMatrix)
         }
     }
 }
