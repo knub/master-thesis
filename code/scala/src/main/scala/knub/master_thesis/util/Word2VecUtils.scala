a5b0d5d7eae8b24e0336d956f63ace004dd7f272
Stefan Bunk <stefan.bunk@googlemail.com>
2016-11-01 17:19:24 +0100

    Finish raw-we-tm.

--- code/scala/src/main/scala/knub/master_thesis/util/Word2VecUtils.scala
+++ code/scala/src/main/scala/knub/master_thesis/util/Word2VecUtils.scala
@@ -5,16 +5,27 @@ import org.deeplearning4j.models.embeddings.wordvectors.WordVectors
 
 object Word2VecUtils {
     def findActualWord(word2Vec: WordVectors, word: String): String = {
-        val actualWord =
-            if (word2Vec.hasWord(word))
-                word
-            else if (word2Vec.hasWord(WordUtils.capitalize(word)))
-                WordUtils.capitalize(word)
-            else if (word2Vec.hasWord(word.toUpperCase))
-                word.toUpperCase()
-            else
+        findActualWordOption(word2Vec, word) match {
+            case Some(w) =>
+                w
+            case None =>
                 throw new RuntimeException(word)
-        actualWord
+        }
+    }
+
+    def findActualWordOption(word2Vec: WordVectors, word: String): Option[String] = {
+        if (word2Vec.hasWord(word))
+            Some(word)
+        else if (word2Vec.hasWord(WordUtils.capitalize(word)))
+            Some(WordUtils.capitalize(word))
+        else if (word2Vec.hasWord(word.toUpperCase))
+            Some(word.toUpperCase())
+        else
+            None
+    }
+
+    def findActualVector(word2Vec: WordVectors, word: String): Option[Array[Double]] = {
+        findActualWordOption(word2Vec, word).map(word2Vec.getWordVector)
     }
 
 }
