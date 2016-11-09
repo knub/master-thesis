d021573400849779ffa1a44b15cb40aa13a3f179
Stefan Bunk <stefan.bunk@googlemail.com>
2016-11-09 22:03:45 +0100

    Add topic webpage

--- webapp/web.py
+++ webapp/web.py
@@ -1,5 +1,30 @@
 from flask import Flask
 from flask import render_template
+from collections import namedtuple
+import random
+from random import shuffle
+
+import os
+
+
+Intrusion = namedtuple('Intrusion', ['method', 'id', 'topic_id', 'words', 'intrusion'])
+
+
+def read_all_intrusions():
+    intrusions_in_files = []
+    for file_name in sorted(os.listdir("out/word-intrusion")):
+        method = file_name.replace(".txt", "")
+        with open("out/word-intrusion/%s" % file_name) as f:
+            for line in f:
+                line = line.rstrip().split("\t")
+                intrusions_in_files.append(Intrusion(method, int(line[0]), int(line[1]), line[-6:-1], line[-1]))
+    return intrusions_in_files
+
+
+intrusions = read_all_intrusions()
+random.seed(21011991)
+shuffle(intrusions)
+
 
 app = Flask(__name__)
 
@@ -7,13 +32,18 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template('hello.html')
-    # return "Hey, you already tagged xxx topics. Continue?"
 
 
 @app.route("/topic/<int:intrusion_id>")
 def intrusion(intrusion_id):
-    return render_template('hello.html')
-    return "Hey, you already tagged xxx topics. Continue?"
+    if intrusion_id >= len(intrusions):
+        return "Thank you, you tagged all word intrusion sets!"
+    else:
+        intrusion = intrusions[intrusion_id]
+        words = [intrusion.intrusion] + intrusion.words
+        random.seed(21011991)
+        shuffle(words)
+        return render_template('topic.html', words=words)
 
 if __name__ == "__main__":
     app.run()
