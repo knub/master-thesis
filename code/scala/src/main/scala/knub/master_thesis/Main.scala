420a46fec9bc0b964fca91e2c930d53d206f54b9
Stefan Bunk <stefan.bunk@googlemail.com>
2016-11-18 13:41:34 +0100

    Change intrusions

--- code/scala/src/main/scala/knub/master_thesis/Main.scala
+++ code/scala/src/main/scala/knub/master_thesis/Main.scala
@@ -878,76 +878,88 @@ object Main {
     def wordIntrusion(args: Args): Unit = {
         val outputFolder = new File("out/word-intrusion/")
         outputFolder.mkdir()
-        val r = new Random(21011991)
 
         val TOP_WORDS = 5
 
         val samples = Map(
 //            // 44.1
-//            "topicvec" ->
-//                "/home/knub/Repositories/topicvec/results/nips.dim-200.alpha-0-02.iterations-500/iteration-500.500.topics",
-//            // 42.1
-//            "welda-gaussian" ->
-//                "/home/knub/Repositories/master-thesis/models/topic-models/topic.nips.50-1500.alpha-0-02.beta-0-02/model.dim-200.skip-gram.embedding.welda.gaussian.redo.topic0-no.pca-10.des-20.lambda-0-2.lambdaact-0-24/welda.iteration-200.topics.500",
-//            // 43.5
+            "topicvec" ->
+                "/home/knub/Repositories/topicvec/results/nips.dim-200.alpha-0-02.iterations-500/iteration-500.500.topics",
+            // 42.1
+            "welda-gaussian" ->
+                "/home/knub/Repositories/master-thesis/models/topic-models/topic.nips.50-1500.alpha-0-02.beta-0-02/model.dim-200.skip-gram.embedding.welda.gaussian.redo.topic0-no.pca-10.des-20.lambda-0-2.lambdaact-0-24/welda.iteration-200.topics.500",
+            // 46.1
+            "welda-gaussian-mixture" ->
+                "/home/knub/Repositories/master-thesis/models/topic-models/topic.nips.50-1500.alpha-0-02.beta-0-02/model.dim-200.skip-gram.embedding.welda.gaussian-mixture.topic0-no.pca-50.des-200.lambda-0-5.lambdaact-0-38/welda.iteration-200.topics.100",
+            // 42.0
+            "lflda" ->
+                "/home/knub/Repositories/master-thesis/models/topic-models/topic.nips.50-1500.alpha-0-02.beta-0-02/lflda.dim-200.lambda-0-6.alphasum-1-0.beta-0-02/iteration-020.500.topics",
+//          // 43.5
 //            "lflda" ->
-//                "/home/knub/Repositories/master-thesis/models/topic-models/topic.nips.50-1500.alpha-0-02.beta-0-02/lflda.dim-50.lambda-0-6.alphasum-1-0.beta-0-02/iteration-100.500.topics",
-//            // 39.8
+//                "/home/knub/Repositories/master-thesis/models/topic-models/topic.nips.50-1500.alpha-0-02.beta-0-02.FIRST/lflda.dim-50.lambda-0-6.alphasum-1-0.beta-0-02/iteration-100.500.topics",
+//          // 39.8
 //            "lda" ->
 //                "/home/knub/Repositories/master-thesis/models/topic-models/topic.nips.50-1500.alpha-0-02.beta-0-02.rerun/model.500.ssv",
 
+            // 42.9
+            "20_topicvec" ->
+                "/home/knub/Repositories/topicvec/results/topics-20.nips.dim-200.alpha-0-05.iterations-500/iteration-500.500.topics",
             // 42.3
             "20_welda-gaussian" ->
                 "/home/knub/Repositories/master-thesis/models/topic-models/topic.nips.20-1500.alpha-0-05.beta-0-05/model.dim-200.skip-gram.embedding.welda.gaussian.topic0-no.pca-10.des-20.lambda-0-2.lambdaact-0-24/welda.iteration-200.topics.500",
             // 42.1
             "20_welda-gaussian-mixture" ->
                 "/home/knub/Repositories/master-thesis/models/topic-models/topic.nips.20-1500.alpha-0-05.beta-0-05/model.dim-200.skip-gram.embedding.welda.gaussian-mixture.topic0-no.pca-10.des-30.lambda-0-2.lambdaact-0-24/welda.iteration-200.topics.500",
-            // 40.1
-            "20_lda" ->
-                "/home/knub/Repositories/master-thesis/models/topic-models/topic.nips.20-1500.alpha-0-05.beta-0-05/model.500.ssv",
-            // 42.9
-            "20_topicvec" ->
-                "/home/knub/Repositories/topicvec/results/topics-20.nips.dim-200.alpha-0-05.iterations-500/iteration-500.500.topics",
             // 41.1
             "20_lflda" ->
-                "/home/knub/Repositories/master-thesis/models/topic-models/topic.nips.20-1500.alpha-0-05.beta-0-05/lflda.dim-200.lambda-0-6.alphasum-1-0.beta-0-05/iteration-300.500.topics"
+                "/home/knub/Repositories/master-thesis/models/topic-models/topic.nips.20-1500.alpha-0-05.beta-0-05/lflda.dim-200.lambda-0-6.alphasum-1-0.beta-0-05/iteration-300.500.topics",
+            // 40.1
+            "20_lda" ->
+                "/home/knub/Repositories/master-thesis/models/topic-models/topic.nips.20-1500.alpha-0-05.beta-0-05/model.500.ssv"
 
 
 
 //            // 43.6
 //            "topicvec-20news" ->
 //                "/home/knub/Repositories/topicvec/results/nips.dim-50.alpha-0-02.iterations-500/iteration-500.500.topics"
-//            // 46.1
-//            "welda-gaussian-mixture" ->
-//                "/home/knub/Repositories/master-thesis/models/topic-models/topic.nips.50-1500.alpha-0-02.beta-0-02/model.dim-200.skip-gram.embedding.welda.gaussian-mixture.topic0-no.pca-50.des-200.lambda-0-5.lambdaact-0-38/welda.iteration-200.topics.100",
         )
 
         var i: Long = System.currentTimeMillis / 1000
 
         samples.toList.sorted.foreach { case (name, fileName) =>
+            val r = new Random(21011991)
+
             println(s"$START_BOLD$name$STOP_BOLD")
             val takeRightValue = if (fileName.contains(".100")) 100 else 500
-            println(s"takeRightValue: $takeRightValue columns: ${Source.fromFile(fileName).getLines().next().split(' ').size}")
+            println(s"takeRightValue: $takeRightValue columns: ${Source.fromFile(fileName).getLines().next().split(' ').length}")
             val topics = Source.fromFile(fileName).getLines().map { line =>
                 line.split(' ').takeRight(takeRightValue).take(50).toSeq
             }.toSeq
+            println(topics.size)
 
             val potentialIntruderWords = topics.flatMap(_.take(10)).toSet
 //            println(s"Longest word: ${potentialIntruderWords.maxBy(_.length)}")
 
-            val lines = topics.indices.toList.map { topicId =>
+            def buildIntrusion(topicId: Int): String = {
                 val exclusionWords = topics(topicId).toSet
                 assert(exclusionWords.size == 50, s"50 != ${exclusionWords.size} in $fileName")
                 val availableWords = potentialIntruderWords.diff(exclusionWords)
 
                 val intrusionPair = (topics(topicId).take(TOP_WORDS), drawWordFromSet(availableWords, r))
 
-                println(s"$topicId\t${intrusionPair._1.mkString(" ")} $START_BOLD${intrusionPair._2}$STOP_BOLD")
+                //                println(s"$topicId\t${intrusionPair._1.mkString(" ")} $START_BOLD${intrusionPair._2}$STOP_BOLD")
                 val result = s"$i\t$topicId\t${intrusionPair._1.mkString("\t")}\t${intrusionPair._2}"
                 i += 1
                 result
             }
-            FileUtils.writeLines(new File(s"${outputFolder.getAbsolutePath}/$name.txt"), "UTF-8", lines.asJavaCollection)
+
+            val lines = topics.indices.toList.map(buildIntrusion)
+            val extra = if (name.endsWith("welda-gaussian")) {
+                List(1).map(buildIntrusion)
+            } else {
+                Nil
+            }
+            FileUtils.writeLines(new File(s"${outputFolder.getAbsolutePath}/$name.txt"), "UTF-8", (lines ::: extra).asJavaCollection)
         }
     }
 
