af178d1a1f0e1738fd3651a6690d1153454f52ed
Stefan Bunk <stefan.bunk@googlemail.com>
2016-12-19 19:33:56 +0100

    More dimensions for dc.

--- code/scala/src/main/scala/knub/master_thesis/Main.scala
+++ code/scala/src/main/scala/knub/master_thesis/Main.scala
@@ -308,6 +308,8 @@ object Main {
                     yield args.copy(
                         modelFileName = "/data/wikipedia/2016-06-21/topic-models/topic.20news.50-1500.alpha-0-02.beta-0-02/model",
                         lambda = lambda,
+                        pcaDimensions = 50,
+                        distributionEstimationSamples = 200,
                         modelNamePrefix = s"welda-gaussian-lambda-dc",
                         embeddingFileName = embedding._1,
                         numDocuments = embedding._2
