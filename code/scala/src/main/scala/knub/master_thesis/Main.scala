b5583818e67d7685c917f844be5c9cc988be3e15
Stefan Bunk <stefan.bunk@googlemail.com>
2016-12-19 23:57:41 +0100

    welda-vmf-lambda-dc

--- code/scala/src/main/scala/knub/master_thesis/Main.scala
+++ code/scala/src/main/scala/knub/master_thesis/Main.scala
@@ -73,6 +73,7 @@ object Main {
             "supply-tm-similarity", "welda-sim",
             "welda-gaussian", "welda-vmf",
             "welda-vmf-pca",
+            "welda-vmf-lambda-dc",
             "welda-lda",
             "welda-gaussian-lambda-dc",
             "welda-gaussian-lambda", "welda-gaussian-random-init",
@@ -315,6 +316,17 @@ object Main {
                         numDocuments = embedding._2
                     )
                 runCases(cases, 30, new GaussianWELDA(_))
+            case "welda-vmf-lambda-dc" =>
+                val lambdas = List(0.0, 0.001, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
+                val cases = for (embedding <- embeddings.take(1); lambda <- lambdas)
+                    yield args.copy(
+                        modelFileName = "/data/wikipedia/2016-06-21/topic-models/topic.20news.50-1500.alpha-0-02.beta-0-02/model",
+                        lambda = lambda,
+                        modelNamePrefix = s"welda-vmf-lambda-dc",
+                        embeddingFileName = embedding._1,
+                        numDocuments = embedding._2
+                    )
+                runCases(cases, 30, new VmfWELDA(_))
             case "welda-gaussian-nips" =>
                 val lambdas = List(0.0, 0.001, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
                 val cases = for (embedding <- nipsEmbeddings; lambda <- lambdas; idx <- 0 until 5)
